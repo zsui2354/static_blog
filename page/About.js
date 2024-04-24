fa240c6689c76b652f6ba550cf19206b9e8d80a9
ZSUI2354 <h2118820285@outlook.com>
2024-04-24 18:16:47 +0800

    Update AboutPage

--- page/About.js
+++ page/About.js
@@ -1,54 +1,66 @@
-document.addEventListener('DOMcontentLoaded',function(){
-    const header = document.querySelector
-    ('header');
-    const container = document.getElementById
-    ('container');
-    const menuButton = documen.getElementById
-    ('menu');
-    const links =document.querySelectorAll
-    ('a[href^="#"]');
 
-    //function to handle the scroll 处理滚动的函数
-    function handleScroll(){
-        container.classList.remove
-        ('menuopen');
-        header.classList.toggle('sticky',window.scrollY >= 100);
-    }
 
-    //Function to handle menu button click  处理菜单按钮点击的函数
-    function handleMenuButtonClick(){
-        header.classList.remove('sticky');
-        container.classList.toggle
-        ('menuopen');
-    }
 
-    //Function to handle anchor links click 处理锚链接点击的函数
-    function handleLinkClick(event){
-        event.preventDefault();
-        const targetId = this.getAttribute
-        ('href');
-        const targetElement = document.querySelectorAll(targetId);
-        if(targetElement){
-            targetElement.scrollIntoView({
-                behavior:'smooth'
-            })
-        }
-    }
+/* 
+    ///     Lightning.js
+    ///
+    ///     Project： https://github.com/zsui2354/LG.js
+    ///     
 
-    //Function to close the menu when clicking outside and show the sticky menu
-    function handleCloseOutside(event){
-        if(!menuButton.contains(event.target)){
-            //Check if the click was outside the menu button
-            container.classList.remove('menuopen');
-            header.classList.add('sticky');
-        }
-    }
 
-    window.addEventListener('scroll', handleScroll);
-    menuButton.addEventListener('click',handleMenuButtonClick);
-    links.forEach(link => link.addEventListener('click',handleLinkClick));
+    @===========================================================================
+    @                               Lightning.js
+    @           An example of a quick call name given to a DOM element
+    @   
+    @   1.Mov_box （class）
+    @
+    @
+    @
+    @
+    @
 
-    //Listen for clicks anywhere in document
-    document.addEventListener('click',handleCloseOutside);
-    
-})
+
+
+*/
+
+
+/*  
+        @  By.author : zsui2354
+        @
+        @  introduce:
+        @  Enter the viewport and trigger dynamic effects when crossing the viewport
+
+
+ */
+        var options = {
+            root: null, // 使用默认的视口作为根
+            rootMargin: '0px', // 视口的边界
+            threshold: 0.5 // 盒子可见度达到50%时触发回调函数
+        };
+        
+        // 创建一个 IntersectionObserver 实例，用于处理交叉状态变化
+        var observer = new IntersectionObserver(function(entries, observer) {
+            // 遍历每个观察到的交叉状态
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    console.log('盒子进入视口范围');
+                    entry.target.style.opacity = '1';
+                    entry.target.style.transform = 'translateY(-30px)';
+                } else {
+                    console.log('盒子离开视口范围');
+                }
+            });
+        }, options);
+        
+        // 监听所有指定的盒子元素
+        var boxes = document.querySelectorAll('.Mov_box');
+        
+        boxes.forEach(function(box) {
+            observer.observe(box);
+            box.style.transition = "all 1.5s ease";
+            box.style.opacity = '0';
+            box.style.transform = 'translateY(100px)';
+        
+        });
+        
+        /* ######################################################################### */
